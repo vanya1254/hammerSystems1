6f9f9b29c70423545ee4a4165f454f9a56b80325
Ivan <vanya1254@yandex.ru>
2024-12-13 10:38:41 +0300

    feat: move some new components

--- src/views/app-views/main/mailings/index.js
+++ src/views/app-views/main/mailings/index.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
+import InnerAppLayout from "layouts/inner-app-layout";
+import MailMenu from "./MailMenu";
+import MailContent from "./MailContent";
 
-const Mailings = () => {
-  return <div>Mailings</div>;
-};
+export class Mailings extends Component {
+  render() {
+    return (
+      <div className="mail">
+        <InnerAppLayout
+          sideContent={<MailMenu {...this.props} />}
+          mainContent={<MailContent {...this.props} />}
+          border
+        />
+      </div>
+    );
+  }
+}
 
 export default Mailings;
