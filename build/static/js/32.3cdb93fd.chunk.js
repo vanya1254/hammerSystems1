1938297051dc21958c0fbaebe76f95f22b58792f
Ivan <vanya1254@yandex.ru>
2024-12-13 11:32:02 +0300

    fix

---
+++ build/static/js/32.3cdb93fd.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpemilus=this.webpackJsonpemilus||[]).push([[32],{573:function(s,e,i){"use strict";i.r(e);i(0);var t=i(5);e.default=()=>Object(t.jsx)("div",{children:"Settings"})}}]);
+//# sourceMappingURL=32.3cdb93fd.chunk.js.map
